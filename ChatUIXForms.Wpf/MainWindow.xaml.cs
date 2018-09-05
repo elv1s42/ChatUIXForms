08af157f4e031f7b13d2dba4621573e87eba4e74
Evgeniy Kosjakov <42_@mail.ru>
2018-09-05 21:01:33 +0300

    versions updated + WPF project added

---
+++ ChatUIXForms.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace ChatUIXForms.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Forms.Init();
+            LoadApplication(new ChatUIXForms.App());
+        }
+    }
+}
